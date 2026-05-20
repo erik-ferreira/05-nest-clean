2af46a77a2210602b863c16f247f3d143f9e5006
erik-ferreira <erikdesouzagf@gmail.com>
2026-05-19 22:24:56 -0300

    create presenter questions

--- src/infra/http/controllers/fetch-recent-questions.controller.ts
+++ src/infra/http/controllers/fetch-recent-questions.controller.ts
@@ -6,6 +6,8 @@ import { ZodValidationPipe } from "@/infra/http/pipes/zod-validation-pipe"
 import { JwtAuthGuard } from "@/infra/auth/jwt-auth.guard"
 import { FetchRecentQuestionsUseCase } from "@/domain/forum/application/use-cases/fetch-recent-questions"
 
+import { QuestionPresenter } from "@/infra/http/presenters/question-presenter"
+
 const pageQueryParamSchema = z.coerce.number().int().min(1).default(1)
 
 type PageQueryParamSchema = z.infer<typeof pageQueryParamSchema>
@@ -21,8 +23,14 @@ export class FetchRecentQuestionsController {
   async handle(
     @Query("page", queryPageValidationPipe) page: PageQueryParamSchema,
   ) {
-    const questions = this.fetchRecentQuestions.execute({ page })
+    const result = await this.fetchRecentQuestions.execute({ page })
 
-    return { questions }
+    if (result.isLeft()) {
+      throw new Error()
+    }
+
+    const questions = result.value.questions
+
+    return { questions: questions.map(QuestionPresenter.toHTTP) }
   }
 }
