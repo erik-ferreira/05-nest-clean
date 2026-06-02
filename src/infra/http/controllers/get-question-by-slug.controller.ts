09f2e8197134dd456f10f2db6933c25966262abd
erik-ferreira <erikdesouzagf@gmail.com>
2026-06-01 22:24:27 -0300

    create controller to get question by slug

---
+++ src/infra/http/controllers/get-question-by-slug.controller.ts
@@ -0,0 +1,21 @@
+import { Get, Controller, BadRequestException, Param } from "@nestjs/common"
+
+import { GetQuestionBySlugUseCase } from "@/domain/forum/application/use-cases/get-question-by-slug"
+
+import { QuestionPresenter } from "@/infra/http/presenters/question-presenter"
+
+@Controller("/questions")
+export class GetQuestionBySlugController {
+  constructor(private getQuestionBySlug: GetQuestionBySlugUseCase) {}
+
+  @Get()
+  async handle(@Param("slug") slug: string) {
+    const result = await this.getQuestionBySlug.execute({ slug })
+
+    if (result.isLeft()) {
+      throw new BadRequestException()
+    }
+
+    return { question: QuestionPresenter.toHTTP(result.value.question) }
+  }
+}
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -5,9 +5,14 @@ import { DatabaseModule } from "@/infra/database/database.module"
 import { AuthenticateController } from "@/infra/http/controllers/authenticate.controller"
 import { CreateAccountController } from "@/infra/http/controllers/create-account.controller"
 import { CreateQuestionController } from "@/infra/http/controllers/create-question.controller"
+import { GetQuestionBySlugController } from "@/infra/http/controllers/get-question-by-slug.controller"
 import { FetchRecentQuestionsController } from "@/infra/http/controllers/fetch-recent-questions.controller"
 
 import { CreateQuestionUseCase } from "@/domain/forum/application/use-cases/create-question"
+import { RegisterStudentUseCase } from "@/domain/forum/application/use-cases/register-student"
+import { GetQuestionBySlugUseCase } from "@/domain/forum/application/use-cases/get-question-by-slug"
+import { AuthenticateStudentUseCase } from "@/domain/forum/application/use-cases/authenticate-student"
+import { FetchRecentQuestionsUseCase } from "@/domain/forum/application/use-cases/fetch-recent-questions"
 
 @Module({
   imports: [DatabaseModule],
@@ -16,7 +21,14 @@ import { CreateQuestionUseCase } from "@/domain/forum/application/use-cases/crea
     AuthenticateController,
     CreateQuestionController,
     FetchRecentQuestionsController,
+    GetQuestionBySlugController,
   ],
-  providers: [CreateQuestionUseCase],
+  providers: [
+    CreateQuestionUseCase,
+    FetchRecentQuestionsUseCase,
+    RegisterStudentUseCase,
+    AuthenticateStudentUseCase,
+    GetQuestionBySlugUseCase,
+  ],
 })
 export class HttpModule {}
