e60a804255fd9cba703c524d91af6aa8e7ce009f
erik-ferreira <erikdesouzagf@gmail.com>
2026-06-23 22:15:20 -0300

    prisma comment with author

---
+++ src/infra/database/prisma/mappers/prisma-comment-with-author-mapper.ts
@@ -0,0 +1,25 @@
+import {
+  User as PrismaUser,
+  Comment as PrismaComment,
+} from "@/generated/prisma/client"
+
+import { UniqueEntityID } from "@/core/entities/unique-entity-id"
+
+import { CommentWithAuthor } from "@/domain/forum/enterprise/entities/value-objects/comment-with-author"
+
+type PrismaCommentWithAuthor = PrismaComment & {
+  author: PrismaUser
+}
+
+export class PrismaCommentWithAuthorMapper {
+  static toDomain(raw: PrismaCommentWithAuthor): CommentWithAuthor {
+    return CommentWithAuthor.create({
+      commentId: new UniqueEntityID(raw.id),
+      authorId: new UniqueEntityID(raw.authorId),
+      author: raw.author.name,
+      content: raw.content,
+      createdAt: raw.createdAt,
+      updatedAt: raw.updatedAt,
+    })
+  }
+}
--- src/infra/database/prisma/repositories/prisma-questions-comments-repository.ts
+++ src/infra/database/prisma/repositories/prisma-questions-comments-repository.ts
@@ -2,10 +2,12 @@ import { Injectable } from "@nestjs/common"
 
 import { PrismaService } from "@/infra/database/prisma/prisma.service"
 import { PrismaQuestionCommentMapper } from "@/infra/database/prisma/mappers/prisma-question-comment-mapper"
+import { PrismaCommentWithAuthorMapper } from "@/infra/database/prisma/mappers/prisma-comment-with-author-mapper"
 
 import { PaginationParams } from "@/core/repositories/pagination-params"
 
 import { QuestionComment } from "@/domain/forum/enterprise/entities/question-comment"
+import { CommentWithAuthor } from "@/domain/forum/enterprise/entities/value-objects/comment-with-author"
 import { QuestionCommentsRepository } from "@/domain/forum/application/repositories/question-comments-repository"
 
 @Injectable()
@@ -38,6 +40,25 @@ export class PrismaQuestionCommentsRepository implements QuestionCommentsReposit
     })
 
     return questionComments.map(PrismaQuestionCommentMapper.toDomain)
+  }
+
+  async findManyByQuestionIdWithAuthor(
+    questionId: string,
+    { page }: PaginationParams,
+  ): Promise<CommentWithAuthor[]> {
+    const questionComments = await this.prisma.comment.findMany({
+      where: { questionId },
+      orderBy: {
+        createdAt: "desc",
+      },
+      include: {
+        author: true,
+      },
+      take: 20,
+      skip: (page - 1) * 20,
+    })
+
+    return questionComments.map(PrismaCommentWithAuthorMapper.toDomain)
   }
 
   async create(questionComment: QuestionComment): Promise<void> {
